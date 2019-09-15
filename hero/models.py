f730bead12fec3a051ec850f89a5500cecf1617c
Danilo Ramos <dgaramos@gmail.com>
2019-09-15 17:22:36 -0300

    Super hero team implementation

--- hero/models.py
+++ hero/models.py
@@ -7,8 +7,20 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=100, choices=(('M', 'Male'), ('F', 'Female')), default='F')
-    movie = models.CharField(max_length=100)
+    mainTeam = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class SuperHeroTeam(models.Model):
+    name = models.CharField(max_length=100)
+    heroes = models.ManyToManyField(Hero)
+    editor = models.CharField(max_length=100, choices=(('Marvel', 'Marvel'), ('DC', 'Detective Comics')), default='Marvel')
+
     def __str__(self):
         return self.name
 
--- hero/schema.py
+++ hero/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from hero.models import Hero
+from hero.models import Hero, SuperHeroTeam
 
 
 class HeroType(DjangoObjectType):
@@ -8,9 +8,16 @@ class HeroType(DjangoObjectType):
         model = Hero
 
 
+class SuperHeroTeamType(DjangoObjectType):
+    class Meta:
+        model = SuperHeroTeam
+
+
 class Query(graphene.ObjectType):
     hero = graphene.Field(HeroType, id=graphene.Int())
+    superHeroTeam = graphene.Field(SuperHeroTeamType, id=graphene.Int())
     heroes = graphene.List(HeroType)
+    superHeroTeams = graphene.List(SuperHeroTeamType)
 
     def resolve_hero(self, info, **kwargs):
         id = kwargs.get('id')
@@ -20,15 +27,33 @@ class Query(graphene.ObjectType):
 
         return None
 
+    def resolve_superHeroTeam(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return SuperHeroTeam.objects.get(pk=id)
+
+        return None
+
     def resolve_heroes(self, info, **kwargs):
         return Hero.objects.all()
+
+    def resolve_superHeroTeams(self, info, **kwargs):
+        return SuperHeroTeam.objects.all()
 
 
 class HeroInput(graphene.InputObjectType):
     id = graphene.ID()
     name = graphene.String()
     gender = graphene.String()
-    movie = graphene.String()
+    mainTeam = graphene.String()
+
+
+class SuperHeroTeamInput(graphene.InputObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    heroes = graphene.List(HeroInput)
+    editor = graphene.String()
 
 
 class CreateHero(graphene.Mutation):
@@ -41,7 +66,7 @@ class CreateHero(graphene.Mutation):
     @staticmethod
     def mutate(root, info, input=None):
         ok = True
-        hero_instance = Hero(name=input.name, gender=input.gender, movie=input.movie)
+        hero_instance = Hero(name=input.name, gender=input.gender, mainTeam=input.mainTeam)
         hero_instance.save()
         return CreateHero(ok=ok, hero=hero_instance)
 
@@ -62,7 +87,7 @@ class UpdateHero(graphene.Mutation):
             ok = True
             hero_instance.name = input.name
             hero_instance.gender = input.gender
-            hero_instance.movie = input.movie
+            hero_instance.mainTeam = input.mainTeam
             hero_instance.save()
             return UpdateHero(ok=ok, hero=hero_instance)
         return UpdateHero(ok=ok, hero=None)
@@ -87,7 +112,81 @@ class DeleteHero(graphene.Mutation):
         return DeleteHero(ok=ok)
 
 
+class CreateSuperHeroTeam(graphene.Mutation):
+    class Arguments:
+        input = SuperHeroTeamInput(required=True)
+
+    ok = graphene.Boolean()
+    superHeroTeam = graphene.Field(SuperHeroTeamType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        heroes = []
+        for hero_input in input.heroes:
+            hero = Hero.objects.get(pk=hero_input.id)
+            if hero is None:
+                return CreateSuperHeroTeam(ok=False, superHeroTeam=None)
+            heroes.append(hero)
+        super_hero_team_instance = SuperHeroTeam(
+          name=input.name,
+          editor=input.editor
+          )
+        super_hero_team_instance.save()
+        super_hero_team_instance.heroes.set(heroes)
+        return CreateSuperHeroTeam(ok=ok, superHeroTeam=super_hero_team_instance)
+
+
+class UpdateSuperHeroTeam(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = SuperHeroTeamInput(required=True)
+
+    ok = graphene.Boolean()
+    superHeroTeam = graphene.Field(SuperHeroTeamType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        super_hero_team_instance = SuperHeroTeam.objects.get(pk=id)
+        if super_hero_team_instance:
+            ok = True
+            heroes = []
+            for hero_input in input.heroes:
+                hero = Hero.objects.get(pk=hero_input.id)
+                if hero is None:
+                    return UpdateSuperHeroTeam(ok=False, movie=None)
+                heroes.append(hero)
+            super_hero_team_instance.name = input.name
+            super_hero_team_instance.editor = input.edit
+            super_hero_team_instance.heroes.set(heroes)
+            return UpdateSuperHeroTeam(ok=ok, superHeroTeam=super_hero_team_instance)
+        return UpdateSuperHeroTeam(ok=ok, superHeroTeam=None)
+
+
+class DeleteSuperHeroTeam(graphene.Mutation):
+    ok = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID()
+
+    ok = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        obj = SuperHeroTeam.objects.get(pk=id)
+        if obj:
+            ok = True
+            obj.delete()
+
+        return DeleteSuperHeroTeam(ok=ok)
+
+
 class Mutation(graphene.ObjectType):
     create_hero = CreateHero.Field()
     update_hero = UpdateHero.Field()
     delete_hero = DeleteHero.Field()
+    create_super_hero_team = CreateSuperHeroTeam.Field()
+    update_super_hero_team = UpdateSuperHeroTeam.Field()
+    delete_super_hero_team = DeleteSuperHeroTeam.Field()
